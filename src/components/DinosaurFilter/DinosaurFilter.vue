7eb779596cbb45cdfb3964a75ce4ff47952bc9d1
martsdag <clarte.lisse.31@gmail.com>
2025-09-16 14:56:39 +0400

    fix style of input

--- src/components/DinosaurFilter/DinosaurFilter.vue
+++ src/components/DinosaurFilter/DinosaurFilter.vue
@@ -7,7 +7,7 @@
       aria-label="Filter dinosaurs by name"
       autocomplete="off"
       enterkeyhint="search"
-      class="p-2 border border-sky-300 rounded w-full"
+      class="p-2 border border-sky-300 rounded w-full bg-transparent focus:outline-none focus:ring-1 focus:ring-sky-500"
       autocapitalize="none"
       spellcheck="false"
       v-model="model"
