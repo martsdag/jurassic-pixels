f719a86b4a220653d4b9e2d6a6a3310a6ea369a7
martsdag <clarte.lisse.31@gmail.com>
2025-09-16 13:08:56 +0400

    fix dino filter

--- src/components/DinosaurFilter/DinosaurFilter.vue
+++ src/components/DinosaurFilter/DinosaurFilter.vue
@@ -1,47 +1,27 @@
 <template>
   <div class="flex justify-between p-4 gap-4">
     <input
-      type="text"
+      id="dino-filter"
+      type="search"
       placeholder="Filter by name..."
+      aria-label="Filter dinosaurs by name"
+      autocomplete="off"
+      enterkeyhint="search"
       class="p-2 border border-sky-300 rounded w-full"
-      v-model="internalValue"
+      v-model="model"
+      @keydown.enter.prevent="emitUpdate"
     />
-    <BaseButton class="flex button--sky items-center justify-center" @click="emitUpdate">
-      <BaseIcon :path="mdiSearchWeb" class="w-6 h-6" />
-    </BaseButton>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import BaseButton from '../BaseButton/BaseButton.vue';
-import { mdiSearchWeb } from '@mdi/js';
-import BaseIcon from '../BaseIcon/BaseIcon.vue';
-
-const props = defineProps<{
-  modelValue: string;
-}>();
+const model = defineModel<string>({ default: '' });
 
 const emit = defineEmits<{
   (e: 'update:modelValue', value: string): void;
 }>();
 
-const internalValue = ref(props.modelValue);
-
-watch(
-  () => props.modelValue,
-  (newVal) => {
-    if (newVal !== internalValue.value) {
-      internalValue.value = newVal;
-    }
-  },
-);
-
-watch(internalValue, (newVal) => {
-  emit('update:modelValue', newVal);
-});
-
 const emitUpdate = () => {
-  emit('update:modelValue', internalValue.value);
+  emit('update:modelValue', model.value);
 };
 </script>
