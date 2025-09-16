da1cf305659673b169511f491b853b7136df69a1
martsdag <clarte.lisse.31@gmail.com>
2025-09-16 17:15:04 +0400

    fix(responsive): improve encyclopedia card display on tablets

--- src/views/Home/ViewHome.vue
+++ src/views/Home/ViewHome.vue
@@ -12,7 +12,7 @@
       </BaseTooltip>
     </div>
 
-    <div class="mt-4 mx-4 md:mt-6 md:mx-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 p-5">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-5 p-5">
       <DinosaurCard
         v-for="dinosaur in filteredDinosaurs"
         :dinosaur
