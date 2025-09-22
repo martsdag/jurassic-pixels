6848f21853ce51983950f06709b28ab51f068e09
martsdag <clarte.lisse.31@gmail.com>
2025-09-22 16:32:20 +0400

    draft: add introduction dialog

---
+++ src/components/IntroModal/IntroModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <BaseDialog
+    :title="'Welcome to JURASSIC PIXELS! 🦖✨'"
+    :buttons="[{ id: 0, text: 'OK', class: 'button--amber', onClick: () => closeDialog() }]"
+    ref="baseDialog"
+  >
+    <p class="text-gray-700 mb-6">
+      JURASSIC PIXELS is an encyclopedia website where you can find all the information about dinosaurs and their
+      locations. Discover more about prehistoric giants and the places where their remains were found.
+    </p>
+
+    <div class="flex flex-col gap-4">
+      <RouterLink
+        :to="{ name: RouteName.Home }"
+        class="bg-sky-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded-lg transition-colors"
+      >
+        View Dinosaurs
+      </RouterLink>
+      <RouterLink
+        :to="{ name: RouteName.DinosaurMapPage }"
+        class="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded-lg transition-colors"
+      >
+        View Map
+      </RouterLink>
+    </div>
+  </BaseDialog>
+</template>
+
+<script setup lang="ts">
+import { onMounted, useTemplateRef } from 'vue';
+import BaseDialog from '../BaseDialog/BaseDialog.vue';
+import { RouterLink } from 'vue-router';
+import { RouteName } from '@/router';
+
+const baseDialog = useTemplateRef('baseDialog');
+
+const closeDialog = () => baseDialog.value?.close();
+
+const openDialog = () => baseDialog.value?.open();
+
+onMounted(() => {
+  openDialog();
+});
+</script>
---
+++ src/components/IntroModal/index.ts
@@ -0,0 +1,1 @@
+export { default } from '@/components/IntroModal/IntroModal.vue';
